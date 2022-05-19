2b36891b69e0ae01e00c1016b1d9ec4f02748121
Robin Skoogh <85128188+robinskoogh@users.noreply.github.com>
2022-05-19 15:01:29 +0200

    easdasd

--- Program.cs
+++ Program.cs
@@ -64,7 +64,8 @@ builder.Services.AddCors(options =>
             policy.AllowAnyMethod();
             policy.WithOrigins(
                 "http://localhost:3000",
-                "http://127.0.0.1:5500");
+                "http://127.0.0.1:5500",
+                "http://192.168.56.1:3000");
         });
 });
 
